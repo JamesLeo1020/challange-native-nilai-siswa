b606d0d3c5d4b55ef1d572c9a09ce80110930efd
james hestu satria <53498600+leo-chan1020@users.noreply.github.com>
2020-04-06 16:16:13 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="http://localhost/haney/aset/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://localhost/haney/aset/fontawesome/css/all.min.css">
+  <title>Input Nilai Siswa</title>
+</head>
+<body style="background-image:linear-gradient(to right
+, #4d089a,#05dfd7
+ );">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>Selamat Datang Di DataBase Data Nilai Siswa</a>
+    <button class="navbar-toggler"type="button"data-toggle="collapse"data-target="#navbarNavAltMarkup">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      
+    </div>
+  </div>
+</nav>
+<script src="http://localhost/haney/aset/jquery.js"></script>
+<script src="http://localhost/haney/aset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
